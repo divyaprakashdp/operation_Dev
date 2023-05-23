1e90ad620365be49742b04b8001e5bab0cc36e4a
divyaprakash <d.prakash1506@gmail.com>
2023-05-23 17:44:10 +0530

    Added JS_Revision

---
+++ JS_Revision/01-variables-datatypes-more/01-console/script.js
@@ -0,0 +1,23 @@
+console.log(100);
+
+console.log("Hello");
+
+console.log(20, 'hello', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('warning');
+
+console.info("info");
+
+console.table({name: 'DP', email: 'dp@gmail.com'})
+
+console.group('simple')
+console.error('Alert');
+console.warn('warning');
+console.info("info");
+console.groupEnd();
