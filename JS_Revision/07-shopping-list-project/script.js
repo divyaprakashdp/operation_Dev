2109c3f69ab170d32e5c5342d929f2a7718d2e80
divyaprakash <d.prakash1506@gmail.com>
2023-06-05 22:50:22 +0530

    added remove feature

--- JS_Revision/07-shopping-list-project/script.js
+++ JS_Revision/07-shopping-list-project/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearAll = document.getElementById('clear')
 
 function addItem(e){
     e.preventDefault();
@@ -41,5 +42,19 @@ function createIcon(classes){
     return icon;
 }
 
+function removeItem(e){
+    if(e.target.parentElement.classList.contains('remove-item')){
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function removeAllItem(){
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 // event listener
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearAll.addEventListener('click', removeAllItem);
