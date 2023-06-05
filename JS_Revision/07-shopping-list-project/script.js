5f7f0385261f8d86157ae55df9b754cb981d9094
divyaprakash <d.prakash1506@gmail.com>
2023-06-05 22:17:48 +0530

    added addIem feaure

---
+++ JS_Revision/07-shopping-list-project/script.js
@@ -0,0 +1,45 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e){
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+
+    //validate input
+    if(newItem.trim() === ''){
+        alert('Item name cannot be empty');
+        itemInput.value="";
+        return;
+
+    }
+
+    //create an item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    const btn = createButton('remove-item btn-link text-red');
+
+    li.appendChild(btn);
+
+    itemList.appendChild(li);
+    itemInput.value="";
+}
+
+function createButton(classes){
+    const button = document.createElement('button');
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark')
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes){
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+
+// event listener
+itemForm.addEventListener('submit', addItem);
