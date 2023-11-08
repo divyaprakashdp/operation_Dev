a4667d6c694c79db3fcfb924ca6ef0658655efb0
divyaprakash <d.prakash1506@gmail.com>
2023-11-08 09:28:58 +0530

    added password generation functionality

--- react_projects/src/components/PasswordGen.jsx
+++ react_projects/src/components/PasswordGen.jsx
@@ -1,29 +1,79 @@
 import * as React from "react";
+import { useState } from "react";
 import "./PasswordGen.css";
-import { IconButton, TextField, Slider, Checkbox } from "@mui/material";
+import {
+  IconButton,
+  TextField,
+  Slider,
+  Checkbox,
+  Typography,
+} from "@mui/material";
 import KeyOutlinedIcon from "@mui/icons-material/KeyOutlined";
+import { useCallback } from "react";
+import { useEffect } from "react";
 
 export default function PasswordGen() {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(10);
+  const [isNumAllowed, setNumAllowed] = useState(false);
+  const [isSpecialCharAllowed, setSpecialCharAllowed] = useState(false);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (isNumAllowed) str += "1234567890";
+    if (isSpecialCharAllowed) str += "~!@#$%^&*_+-=";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, isNumAllowed, isSpecialCharAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, isNumAllowed, isSpecialCharAllowed, passwordGenerator]);
+
   return (
     <main className="container">
       <header>
-        <h1>Password Generator</h1>
+        <Typography variant="h4" align="center" marginBottom={"20px"}>
+          Password Generator
+        </Typography>
       </header>
       <div>
-        <TextField id="out" labellined-basic="Outlined" variant="outlined" />
-        <IconButton aria-label="generate">
-          <KeyOutlinedIcon />
+        <TextField
+          sx={{ width: "90%", background: "white" }}
+          id="out"
+          labellined-basic="Outlined"
+          variant="outlined"
+          value={password}
+        />
+        <IconButton aria-label="generate" onClick={passwordGenerator}>
+          <KeyOutlinedIcon fontSize="large" />
         </IconButton>
 
         <div id="features-control">
           <Slider
-            size="small"
-            defaultValue={10}
+            sx={{ width: "50%" }}
+            defaultValue={length}
             aria-label="Small"
             valueLabelDisplay="auto"
+            onChange={(event) => {
+              setLength(event.target.value);
+            }}
           />
-          <Checkbox name="Numerals" />
-          <Checkbox label="Specail Char" />
+          <Checkbox
+            defaultChecked={isNumAllowed}
+            onChange={() => setNumAllowed((prev) => !prev)}
+          />
+          <Typography variant="h6">Numerals</Typography>
+          <Checkbox
+            defaultChecked={isSpecialCharAllowed}
+            onChange={() => setSpecialCharAllowed((prev) => !prev)}
+          />
+          <Typography variant="h6">SpecialChar</Typography>
         </div>
       </div>
     </main>
